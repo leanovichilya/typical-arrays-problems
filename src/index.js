df1f6645b50df7868b99ecda1467c5d1aca14740
Ilya Leanovich <leanovich.ilya@gmail.com>
2020-02-23 00:11:02 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    return !Array.isArray(array) || array.length === 0 ? 0
+        : array
+            .reduce((min, item) => item < min ? min = item
+                : min, min = array[0]);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !Array.isArray(array) || array.length === 0 ? 0
+        : array
+            .reduce((max, item) => item > max ? max = item
+                : max, max = array[0]);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return !Array.isArray(array) || array.length === 0 ? 0
+        : array
+        .reduce((sum, item) => sum + item, 0) / array.length
+        .toFixed(1);
+};
